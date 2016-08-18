7916194636b6896b5f8eaef2f72cdb5893cf0f99
Carrison57 <calebarrison@gmail.com>
2016-08-18 08:17:06 -0400

    3 tests running

--- Coin_Changer.rb
+++ Coin_Changer.rb
@@ -1,8 +1,14 @@
 def coin_changer(currency)
-	
-	if currency.keys.include?("dime")
-		"dime"
-	end
+	coins_value = {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
 
+	correct_change = {:penny => 0, :nickel => 0, :dime => 0, :quarter => 0}
 
+		coins_value.each do |coin, value|
+			while 
+				value <= currency
+				correct_change[coin] += 1
+				currency -= value
+			end
+		end
+	correct_change
 end
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -3,13 +3,15 @@ require_relative "coin_changer.rb"
 
 class Test_Coin_Changer < Minitest::Test
 
-	def test_coin_changer_return_minimum_coins
-		currency = {"penny" => "1", "nickel" => "5", "dime" => "10", "quarter" => "25"}
-		assert_equal(true, coin_changer(currency))
+	def	test_13_returns_1_dime_3_pennies
+	assert_equal({:quarter => 0, :dime => 1, :nickel => 0, :penny => 3}, coin_changer(13))
 	end
 
-	def test_coin_changer_if_result_is_a_dime
-		currency = {"penny" => "1", "nickel" => "5", "dime" => "10", "quarter" => "25"}
-		assert_equal("dime", coin_changer(currency))
+	def	test_1027_returns_41_quarters_2_pennies
+	assert_equal({:quarter => 41, :dime => 0, :nickel => 0, :penny => 2}, coin_changer(1027))
+	end
+
+	def	test_6400_returns__dimes_3_pennies
+	assert_equal({:quarter => 256, :dime => 0, :nickel => 0, :penny => 0}, coin_changer(6400))
 	end
 end

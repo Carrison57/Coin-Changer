9e20ccf3fabe0074a5cc9a810e397522d1d42ac4
Carrison57 <calebarrison@gmail.com>
2016-08-25 11:18:07 -0400

    difined quarter, dime, nickel

--- new_coin_changer_test.rb
+++ new_coin_changer_test.rb
@@ -33,4 +33,16 @@ class Test_Coin_Changer < Minitest::Test
 	def test_sixteen_cents_returns_hash_with_one_dime_one_nickel_one_penny
 		assert_equal({dime: 1, nickel: 1}, generate_change(15))
 	end
+
+	def test_twenty_cents_returns_hash_with_two_dimes
+		assert_equal({dime: 2}, generate_change(20))
+	end
+
+	def test_twenty_five_cents_returns_hash_with_two_dimes_two_pennies
+		assert_equal({quarter: 1}, generate_change(25))
+	end
+
+	def test_thirty_cents_returns_hash_with_one_quarter_one_nickel
+		assert_equal({quarter: 1, nickel: 1}, generate_change(30))
+	end
 end
